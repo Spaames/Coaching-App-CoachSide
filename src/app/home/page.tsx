c63a3518a4174850fa7d5359ee238ca7cb6d98c6
Rémi Dupuy <rdu@rdu-mbp.home>
2024-09-19 17:45:42 +0200

    first tab

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,11 +1,37 @@
 'use client'
 
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tr,
+    Th,
+    Td,
+    TableContainer,
+} from '@chakra-ui/react'
 
 export default function Page() {
 
     return (
-        <>
-            HOME PAGE
-        </>
+        <TableContainer>
+            <Table variant='simple' size='lg'>
+                <Thead>
+                    <Tr>
+                        <Th>Athlete(s)</Th>
+                        <Th>Actual Block</Th>
+                        <Th>Last Update</Th>
+                        <Th>Actions</Th>
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    <Tr>
+                        <Td>inches</Td>
+                        <Td>millimetres (mm)</Td>
+                        <Td>25.4</Td>
+                        <Td>bite</Td>
+                    </Tr>
+                </Tbody>
+            </Table>
+        </TableContainer>
     );
 }
