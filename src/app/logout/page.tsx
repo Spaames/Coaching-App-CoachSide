65856eca9fbf90c31bb07896667efabf9ca454c2
Rémi Dupuy <rdu@mac.home>
2024-11-07 11:24:55 +0100

    fixed logout

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -1,13 +1,21 @@
-"use client"
+"use client";
 
-import {useAppDispatch} from "@/app/redux/hooks";
-import {logout} from "@/app/redux/features/authSlice";
-import {useRouter} from "next/navigation";
+import { useEffect } from "react";
+import { useAppDispatch } from "@/app/redux/hooks";
+import { logout } from "@/app/redux/features/authSlice";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
     const dispatch = useAppDispatch();
     const router = useRouter();
-    dispatch(logout());
-    localStorage.removeItem("user");
-    router.push("/login");
+
+    useEffect(() => {
+        dispatch(logout());
+        if (typeof window !== "undefined") {
+            localStorage.removeItem("user");
+        }
+        router.push("/login");
+    }, [dispatch, router]);
+
+    return null;
 }
